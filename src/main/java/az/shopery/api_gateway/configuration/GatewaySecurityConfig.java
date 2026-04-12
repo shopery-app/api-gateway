ceb3b28eac7620f8840833be91e41c5ca58f04bc
Jahangir Alisoy <jahangir.alisoy.std@bhos.edu.az>
2026-04-12 18:50:22 +0400

    issue-5: cors configuration

--- src/main/java/az/shopery/api_gateway/configuration/GatewaySecurityConfig.java
+++ src/main/java/az/shopery/api_gateway/configuration/GatewaySecurityConfig.java
@@ -2,6 +2,7 @@ package az.shopery.api_gateway.configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
@@ -12,6 +13,7 @@ public class GatewaySecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) {
         httpSecurity
+                .cors(Customizer.withDefaults())
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth -> auth.anyRequest().permitAll());
         return httpSecurity.build();
